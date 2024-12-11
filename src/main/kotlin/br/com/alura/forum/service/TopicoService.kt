f78f2cb302e299276a9e1b2a9b38d2bc8f0bb65a
Rodrigo Nobre <roinobre@gmail.com>
2024-12-11 10:59:21 -0300

    first commit

---
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,62 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.mapper.TopicoFormMapper
+import br.com.alura.forum.mapper.TopicoViewMapper
+import br.com.alura.forum.model.Topico
+import org.springframework.stereotype.Service
+import java.util.*
+import java.util.stream.Collectors
+
+@Service
+class TopicoService(
+    private var topicos: List<Topico> = ArrayList(),
+
+    private val topicoViewMapper: TopicoViewMapper,
+    private val topicoFormMapper: TopicoFormMapper
+    ) {
+
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map {
+            t ->  topicoViewMapper.map(t)
+        }.collect(Collectors.toList())
+    }
+
+    fun buscarPorId(id: Long): TopicoView {
+        val topico = topicos.stream().filter({
+            t -> t.id == id
+        }).findFirst().get()
+        return topicoViewMapper.map(topico)
+    }
+
+    fun cadastrar(form: NovoTopicoForm) {
+        val topico = topicoFormMapper.map(form)
+        topico.id = topicos.size.toLong() + 1
+        topicos = topicos.plus(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm) {
+        val topico = topicos.stream().filter({
+                t -> t.id == form.id
+        }).findFirst().get()
+        topicos = topicos.minus(topico).plus(Topico(
+            id = form.id,
+            titulo = form.titulo,
+            mensagem = form.mensagem,
+            autor = topico.autor,
+            curso = topico.curso,
+            respostas = topico.respostas,
+            status = topico.status,
+            dataCriacao = topico.dataCriacao
+        ))
+    }
+
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter({
+                t -> t.id == id
+        }).findFirst().get()
+        topicos = topicos.minus(topico)
+    }
+}
